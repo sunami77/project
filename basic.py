6121dc2f3f2d0b14d97fd02404145c534a131112
race <rfox@vbainfo.com>
2020-02-21 20:22:11 +0000

    first

--- basic.py
+++ basic.py
@@ -2,7 +2,7 @@ from gpiozero import Robot
 from time import sleep
 robot = Robot(left=(4, 14), right=(17, 18))
 i = 1
-robot.forward(speed=0.5)
+robot.forward(speed=1.0)
 print (i, " Going Forward")
 sleep(5)
 robot.right()
